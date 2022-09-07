9c160fb66b3d45cfb703cd21abfe72dc9693193b
Zhiying Fan <zhiying@keyst.one>
2022-09-07 14:26:06 +1000

    Add getSignature

--- Sources/URRegistry/URRegistry.swift
+++ Sources/URRegistry/URRegistry.swift
@@ -15,6 +15,9 @@ public class URRegistry {
     
     private init() {}
     
+    /// Get a CryptoHDKey instance provided by a UR
+    /// - Parameter ur: An UR string
+    /// - Returns: An instance of CryptoHDKey
     public func getHDKey(from ur: String) -> CryptoHDKey? {
         let decoderPointer = UnsafeMutableRawPointer(mutating: decoderPtr)
         let urPointer = UnsafeMutableRawPointer(mutating: (ur as NSString).utf8String)
@@ -22,16 +25,16 @@ public class URRegistry {
         
         URRegistryFFI.ur_decoder_receive(decoderPointer, urPointer)
         
-        let isCompleted = URRegistryFFI.ur_decoder_is_complete(decoderPointer).pointee.value._boolean
+        let isCompleted = URRegistryFFI.ur_decoder_is_complete(decoderPointer).pointee.safeValue?._boolean ?? false
         
         guard isCompleted else { return nil }
         
-        let hdKeyPtr = URRegistryFFI.ur_decoder_resolve(decoderPointer, targetPointer).pointee.value._object
+        let hdKeyPtr = URRegistryFFI.ur_decoder_resolve(decoderPointer, targetPointer).pointee.safeValue?._object
         let hdKeyPointer = UnsafeMutableRawPointer(mutating: hdKeyPtr)
-                    
-        let keyPtr = URRegistryFFI.crypto_hd_key_get_key_data(hdKeyPointer).pointee.value._string
-        let chainCodePtr = URRegistryFFI.crypto_hd_key_get_chain_code(hdKeyPointer).pointee.value._string
-        let sourceFingerprintPtr = URRegistryFFI.crypto_hd_key_get_source_fingerprint(hdKeyPointer).pointee.value._string
+        
+        let keyPtr = URRegistryFFI.crypto_hd_key_get_key_data(hdKeyPointer).pointee.safeValue?._string
+        let chainCodePtr = URRegistryFFI.crypto_hd_key_get_chain_code(hdKeyPointer).pointee.safeValue?._string
+        let sourceFingerprintPtr = URRegistryFFI.crypto_hd_key_get_source_fingerprint(hdKeyPointer).pointee.safeValue?._string
         
         guard
             let keyPtr = keyPtr,
@@ -46,14 +49,20 @@ public class URRegistry {
         return CryptoHDKey(key: key, chainCode: chainCode, sourceFingerprint: sourceFingerprint)
     }
     
+    /// Uncompress public key
+    /// - Parameter compressedKey: Compressed public key
+    /// - Returns: The uncompressed public key
     public func getUncompressedKey(from compressedKey: String) -> String? {
         let keyPointer = UnsafeMutableRawPointer(mutating: (compressedKey as NSString).utf8String)
-        let keyPtr = URRegistryFFI.crypto_hd_key_get_uncompressed_key_data(keyPointer).pointee.value._string
+        let keyPtr = URRegistryFFI.crypto_hd_key_get_uncompressed_key_data(keyPointer).pointee.safeValue?._string
         
         guard let keyPtr = keyPtr else { return nil }
         return String(cString: keyPtr)
     }
-    
+        
+    /// Generate a QR code value which is a UR for hardware wallet to sign
+    /// - Parameter signRequest: A KeystoneSignRequest holding all of the required information
+    /// - Returns: An unsigned UR
     public func requestSign(signRequest: KeystoneSignRequest) -> String? {
         let requestIdPointer = UnsafeMutableRawPointer(mutating: (signRequest.requestId as NSString).utf8String)
         let signDataPointer = UnsafeMutableRawPointer(mutating: (signRequest.signData as NSString).utf8String)
@@ -72,14 +81,52 @@ public class URRegistry {
             originPointer
         )
 
-        let ethSignRequestPtr = ethSignRequest?.pointee.value._object
+        let ethSignRequestPtr = ethSignRequest?.pointee.safeValue?._object
         let ethSignRequestPointer = UnsafeMutableRawPointer(mutating: ethSignRequestPtr)
-        let urEncoderPtr = URRegistryFFI.eth_sign_request_get_ur_encoder(ethSignRequestPointer).pointee.value._object
+        let urEncoderPtr = URRegistryFFI.eth_sign_request_get_ur_encoder(ethSignRequestPointer).pointee.safeValue?._object
         let urEncoderPointer = UnsafeMutableRawPointer(mutating: urEncoderPtr)
-        let qrValuePtr = URRegistryFFI.ur_encoder_next_part(urEncoderPointer).pointee.value._string
+        let qrValuePtr = URRegistryFFI.ur_encoder_next_part(urEncoderPointer).pointee.safeValue?._string
         
         guard let qrValuePtr = qrValuePtr else { return nil }
         
         return String(cString: qrValuePtr).uppercased()
+    }
+    
+    /// Get signature information provided by a UR
+    /// - Parameter ur: An UR string
+    /// - Returns: The hex string of the signature
+    public func getSignature(from ur: String) -> String? {
+        let decoderPointer = UnsafeMutableRawPointer(mutating: decoderPtr)
+        let urPointer = UnsafeMutableRawPointer(mutating: (ur as NSString).utf8String)
+        let targetPointer = UnsafeMutableRawPointer(mutating: ("eth-signature" as NSString).utf8String)
+        
+        URRegistryFFI.ur_decoder_receive(decoderPointer, urPointer)
+        
+        let isCompleted = URRegistryFFI.ur_decoder_is_complete(decoderPointer).pointee.safeValue?._boolean ?? false
+        
+        guard isCompleted else { return nil }
+        
+        let ethSignaturePtr = URRegistryFFI.ur_decoder_resolve(decoderPointer, targetPointer).pointee.safeValue?._object
+        let ethSignaturePtrPointer = UnsafeMutableRawPointer(mutating: ethSignaturePtr)
+        
+        let signaturePtr = URRegistryFFI.eth_signature_get_signature(ethSignaturePtrPointer).pointee.safeValue?._string
+        
+        guard
+            let signaturePtr = signaturePtr
+        else { return nil }
+        
+        let signature = String(cString: signaturePtr)
+        
+        return signature
+    }
+}
+
+extension Response {
+    var safeValue: Value? {
+        if status_code == 0 {
+            return value
+        } else {
+            fatalError("Error: \(String(cString: error_message))")
+        }
     }
 }
--- Tests/URRegistryTests/URRegistryTests.swift
+++ Tests/URRegistryTests/URRegistryTests.swift
@@ -19,24 +19,24 @@ final class URRegistryTests: XCTestCase {
     
     func testGetUncompressedKeyFromEvenY() {
         let compressedKey = "02fef03a2bd3de113f1dc1cdb1e69aa4d935dc3458d542d796f5827abbb1a58b5e"
-        let uncompressedKey = "04fef03a2bd3de113f1dc1cdb1e69aa4d935dc3458d542d796f5827abbb1a58b5ebdffecfa6587da3216d50114700e5e314650cc2268e9fcb6ac31593bcc71d178"
+        let expectedResult = "04fef03a2bd3de113f1dc1cdb1e69aa4d935dc3458d542d796f5827abbb1a58b5ebdffecfa6587da3216d50114700e5e314650cc2268e9fcb6ac31593bcc71d178"
         
         let sut = URRegistry.shared
         
         let result = sut.getUncompressedKey(from: compressedKey)
         
-        XCTAssertEqual(result, uncompressedKey)
+        XCTAssertEqual(result, expectedResult)
     }
     
     func testGetUncompressedKeyFromOddY() {
         let compressedKey = "03b7db1c60fed9f333a5afb0f945c4fafc7739775bc4bda24ac6979362eca0f1f2"
-        let uncompressedKey = "04b7db1c60fed9f333a5afb0f945c4fafc7739775bc4bda24ac6979362eca0f1f2ae4a073a1eb2f8bad6ddb7bcee0c475456d0c490eec0913c7bc30826fff3193d"
+        let expectedResult = "04b7db1c60fed9f333a5afb0f945c4fafc7739775bc4bda24ac6979362eca0f1f2ae4a073a1eb2f8bad6ddb7bcee0c475456d0c490eec0913c7bc30826fff3193d"
         
         let sut = URRegistry.shared
         
         let result = sut.getUncompressedKey(from: compressedKey)
         
-        XCTAssertEqual(result, uncompressedKey)
+        XCTAssertEqual(result, expectedResult)
     }
     
     func testRequestSign() {
@@ -55,6 +55,17 @@ final class URRegistryTests: XCTestCase {
         let sut = URRegistry.shared
         
         let result = sut.requestSign(signRequest: signRequest)
+        
+        XCTAssertEqual(result, expectedResult)
+    }
+    
+    func testGetSignature() {
+        let ur = "UR:ETH-SIGNATURE/OEADTPDAGDEEEODYFPEHETENESDPECFEECEHDPEEFYAOHDFPVLTNFGFHTKSPMSTOHHIHEEIHGSNLPEPLFLPMHKSOWZLSPFVOMEDWZCGTLTWTJYGTKIMYWKHLESMTGHHHZORPZEFERHURBSHDRLLUCWSOLYHTSODYGUAYCSSPBGBKGMUTCWZOQZRDGT"
+        let expectedResult = "e3da463fcfc897ce5c6534654c99afae47ad59c9f283b0e2912cfd4d87f0744d7d8ff45d3996545cfbb6fe45b9df0f58b78b1bc9815ac930530818c8120a52dd1b"
+        
+        let sut = URRegistry.shared
+        
+        let result = sut.getSignature(from: ur)
         
         XCTAssertEqual(result, expectedResult)
     }
